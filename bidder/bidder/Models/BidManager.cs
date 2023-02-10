a3979a6e57e7cf7dff145e3ce3660905e6d8fbda
Raleigh Desmond <95951774+RaleighD@users.noreply.github.com>
2023-02-10 13:26:17 -0500

    Update BidManager.cs

--- bidder/bidder/Models/BidManager.cs
+++ bidder/bidder/Models/BidManager.cs
@@ -3,6 +3,7 @@ namespace bidder.Models
 {
 	public class BidManager
 	{
+		private int IdSeed;
 		private int maxBids;
 		private int numBids;
 		private Bid[] bidList;
@@ -10,11 +11,34 @@ namespace bidder.Models
 		
 		public BidManager(int size)
 		{
+			IdSeed = 0;
 			this.maxBids = size;
 			numBids = 0;
 			bidList = new Bid[maxBids];
 		}
 
+		public int getMaxBids()
+		{
+			return maxBids;
+		}
+
+		public int getNumBids()
+		{
+			return numBids;
+		}
+
+		public Bid[] getBidList()
+		{
+			return bidList;
+		}
+
+		public Boolean addBid(Auction auction, User user, int amount)
+		{
+			Bid temp = new Bid(IdSeed, auction, user, amount);
+			bidList
+
+				//testing 
+		}
 
 
 
