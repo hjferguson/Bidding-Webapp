5ac07f97945837a91dfed83ca95a8e792111cb0f
Raleigh Desmond <95951774+RaleighD@users.noreply.github.com>
2023-02-25 11:26:34 -0500

    Initial DB initialization v2

---
+++ bidder/bidder/Data/SiteContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using bidder.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bidder.Data
+{
+    public class SiteContext : DbContext
+    {
+        public SiteContext(DbContextOptions<SiteContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Auction>().HasData(
+                new Auction(01, "Buffalo", 01));
+
+            modelBuilder.Entity<User>().HasData(
+                new User(1, "mikeWheeler", "password", "mikewheeler@gmail.com"));
+
+
+        }
+    }
+}
+
