0e09105f1d4541afc7c46e2b7f948c3786219b0f
Raleigh Desmond <95951774+RaleighD@users.noreply.github.com>
2023-02-28 10:15:45 -0500

    Updates to the OOP Design
    Minor tweaks to work a little better with the program reqs

--- bidder/bidder/Models/User.cs
+++ bidder/bidder/Models/User.cs
@@ -11,6 +11,7 @@ namespace bidder.Models
 		public string? username { get; set; }
 		public string? password { get; set; }
 		public bool verifiedStatus { get; set;}
+		public string? type { get; set; }
 		public string? email { get; set; }
 
 
@@ -27,37 +28,6 @@ namespace bidder.Models
 		}
 
 		public User() { }
-
-
-		public void setVerified()
-		{
-			verifiedStatus = true;
-		}
-
-		public string getUsername()
-		{
-			return username;
-		}
-
-		public int getId()
-		{
-			return userID;
-		}
-
-		public string getPassword()
-		{
-			return password;
-		}
-
-		public string getEmail()
-		{
-			return email;
-		}
-
-		public bool getStatus()
-		{
-			return verifiedStatus;
-		}
 
 
 
