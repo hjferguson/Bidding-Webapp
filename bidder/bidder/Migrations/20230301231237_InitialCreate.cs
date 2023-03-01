3352eaa71640c12d10e9bee6ca18a3334aca1801
Harlan Ferguson <harlanjazz@gmail.com>
2023-03-01 18:45:51 -0500

    email now works

---
+++ bidder/bidder/Migrations/20230301231237_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bidder.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "endTime", "startTime" },
+                values: new object[] { new DateTime(2023, 3, 1, 18, 12, 37, 594, DateTimeKind.Local).AddTicks(1294), new DateTime(2023, 3, 1, 18, 12, 37, 594, DateTimeKind.Local).AddTicks(1260) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "endTime", "startTime" },
+                values: new object[] { new DateTime(2023, 3, 1, 1, 6, 17, 401, DateTimeKind.Local).AddTicks(9137), new DateTime(2023, 3, 1, 1, 6, 17, 401, DateTimeKind.Local).AddTicks(9104) });
+        }
+    }
+}
