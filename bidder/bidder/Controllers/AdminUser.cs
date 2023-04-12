c8160afa7a254d9a4493d961160ed313da296627
OwenB-jng <owenbeattie1@gmail.com>
2023-04-12 14:29:38 -0400

    admin users / items page, next is button functionality / database update for users

---
+++ bidder/bidder/Controllers/AdminUser.cs
@@ -0,0 +1,23 @@
+﻿using bidder.Data;
+using bidder.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+namespace bidder.Controllers
+
+{
+    public class AdminUser : Controller
+    {
+        public AdminUser(SiteContext cont)
+        {
+            context = cont;
+        }
+        private SiteContext context;
+        public IActionResult Index()
+        {
+            var users = context.Users;
+            return View(users);
+        }
+
+    }
+}
