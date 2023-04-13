c009ba93bc6321e1473e556a260c4dcad4f5f5b3
Harlan Ferguson <harlanjazz@gmail.com>
2023-04-13 18:22:09 -0400

    general search works, categories don't yet

--- bidder/bidder/Controllers/HomeController.cs
+++ bidder/bidder/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using bidder.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Linq;
 
 namespace bidder.Controllers
 {
@@ -20,7 +21,7 @@ namespace bidder.Controllers
             context = cont;
         }
         private SiteContext context;
-        public IActionResult Index()
+        public IActionResult Index(string keyword = "", string category = "", string status = "")
         {
             //Reads if a cookie exists, then finds the user and saves it in the viewbag with name user, entire db object saved
             if (Request.Cookies.ContainsKey("user_credentials"))
@@ -30,9 +31,48 @@ namespace bidder.Controllers
                 ViewBag.user = user;
             }
 
-            var auctions = context.Auctions;
-            return View(auctions);
+            var auctions = context.Auctions.AsQueryable();
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                auctions = auctions.Where(a => a.itemName.Contains(keyword) || a.itemDescription.Contains(keyword));
+            }
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                switch (category)
+                {
+                    case "itemName":
+                        auctions = auctions.OrderBy(a => a.itemName);
+                        break;
+                    case "condition":
+                        auctions = auctions.OrderBy(a => a.condition);
+                        break;
+                    case "type":
+                        auctions = auctions.OrderBy(a => a.type);
+                        break;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(status))
+            {
+                switch (status)
+                {
+                    case "startingBid":
+                        auctions = auctions.OrderBy(a => a.startingBid);
+                        break;
+                    case "currentBid":
+                        auctions = auctions.OrderBy(a => a.currentBid);
+                        break;
+                    case "winningBid":
+                        auctions = auctions.OrderByDescending(a => a.currentBid);
+                        break;
+                }
+            }
+
+            return View(auctions.ToList());
         }
+
 
 
 
